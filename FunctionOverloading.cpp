d563315efaabc12a1ca0652588c13a65cbfda1b9
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 <Asim Ayaz>
2026-06-06 10:27:35 +0500

    Code for FUN

--- FunctionOverloading.cpp
+++ FunctionOverloading.cpp
@@ -11,32 +11,20 @@
 #include <string>
 using namespace std;
 
-// ============================================
-// Three versions of "add" - same name, different parameters
-// ============================================
-
-// Version 1: add two integers
 int add(int a, int b) {
     cout << "Calling int add: ";
     return a + b;
 }
 
-// Version 2: add two doubles
 double add(double a, double b) {
     cout << "Calling double add: ";
     return a + b;
 }
 
-// Version 3: add three integers
 int add(int a, int b, int c) {
     cout << "Calling three-int add: ";
     return a + b + c;
 }
-
-// ============================================
-// Three versions of "print" - same name, different types
-// ============================================
-
 void print(int x) {
     cout << "Integer: " << x << endl;
 }
@@ -52,7 +40,6 @@ void print(string s) {
 int main() {
     cout << "=== Function Overloading Demo ===" << endl;
     
-    // Compiler picks right version based on arguments
     cout << add(3, 4) << endl;           // Calls int add(int, int)
     cout << add(3.5, 4.2) << endl;       // Calls double add(double, double)
     cout << add(1, 2, 3) << endl;        // Calls int add(int, int, int)
@@ -61,9 +48,6 @@ int main() {
     print(42);                           // Calls print(int)
     print(3.14);                         // Calls print(double)
     print("Hello");                      // Calls print(string)
-    
-    // Why overload? Same operation, different data types
-    // No need to remember: addInt, addDouble, addThree...
-    
+        
     return 0;
 }
